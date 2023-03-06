c00eea6c0c91c49bffea4cac2ab6cd0ad41f75f5
Danish-chouhan <danishchouhan41@gmail.com>
2023-03-06 23:52:46 +0530

    BasicQuestions

--- js/allLearned.js
+++ js/allLearned.js
@@ -218,3 +218,77 @@ function mulFn() {
 // Array.prototype.danish = danish
 
 // -----------------------------------------------
+
+// 46 ==> Word without First Character
+//     Create a function that takes a word and returns the new word without including the first character.
+
+function newWord(str){
+    //  return str.slice(1)
+   let a = "";
+   for(let i = 1; i < str.length ; i++){
+      a += str[i]
+   }
+   return a
+}
+
+// console.log(newWord("apple"))
+// console.log(newWord("cherry"))
+// console.log(newWord("plum"))
+
+// -----------------------------------------------
+
+// 47 ==> Concatenating Two Integer Arrays
+//     Create a function to concatenate two integer arrays.
+
+function concat(arr1,arr2){
+    //  return arr1.concat(arr2);
+    let a = arr1
+    for(let i = 0 ; i < arr2.length; i++ ){
+        a.push(arr2[i])
+    }
+    return a
+}
+
+// console.log(concat([1, 3, 5], [2, 6, 8]))
+// console.log(concat([7, 8], [10, 9, 1, 1, 2]))
+// console.log(concat([4, 5, 1], [3, 3, 3, 3, 3]))
+
+// -----------------------------------------------
+
+// 48 ==> Find the Index (Part 1)
+//     Create a function that finds the index of a given item. If the item is not present, return -1.
+function search(arr,item){
+    // return arr.indexOf(item)
+    // return arr.findIndex(a => a == item)
+    for(let i = 0;i < arr.length;i++){
+        if(arr[i] == item){
+            return i
+        }
+    }
+    return -1
+}
+
+// console.log(search([1, 5, 3], 5))
+// console.log(search([9, 8, 3], 3))
+// console.log(search([1, 2, 3], 4))
+// console.log(search(["hi", "edabit", "fgh", "abc"], "fgh"))
+
+// -----------------------------------------------
+
+// 50 ==> Check if an Array Contains a Given Number
+//     Write a function to check if an array contains a particular number.
+function check(arr,num){
+    for(var i = 0 ; i < arr.length ; i++){
+        if(arr[i] == num){
+            return true
+        }
+    }
+    return false
+}
+
+// console.log(check([1, 2, 3, 4, 5], 3))
+// console.log(check([1, 1, 2, 1, 1], 3))
+// console.log(check([5, 5, 5, 6], 5))
+// console.log(check([], 5))
+
+// -----------------------------------------------
