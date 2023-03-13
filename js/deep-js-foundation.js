d01fa73917b8c32aa2979af91e753dfd111028b8
Danish-chouhan <danishchouhan41@gmail.com>
2023-03-13 18:28:44 +0530

    Basic-Js

---
+++ js/deep-js-foundation.js
@@ -0,0 +1,149 @@
+// var a = 10
+// console.log(a) // 10
+// console.log(a++)// 10
+// console.log(a) // 11
+// console.log(++a) //12
+// console.log(a) //12
+
+// var b = "10"
+// b++ 
+// console.log(b) // 11
+
+
+// primitive  
+
+// string
+// Number
+// Boolean
+// undefined
+// null
+// BigInt
+// Symbol
+
+
+
+// non primitive are objects
+
+// non primitive
+
+// Array
+// function 
+// object   
+
+
+// let x = {
+//     name : "Danish",
+// }
+// let y = {
+//     name : "Danish"
+// }
+
+// console.log(x == y) // false
+// console.log(x === y) // false
+
+
+// console.log(typeof (typeof x)) // object
+
+
+// let x ;
+// console.log(typeof NaN) // why it is a number
+// console.log(Number(null))
+// console.log(Number(undefined))
+
+// var a = [1,2,3,4,5]
+// console.log(String(a)) // '[1,2,3]'
+
+// // [nul] -> '[nul]'  
+
+// '' 
+// var x = [null]
+// console.log(String(x));
+// var y = (String(x))
+// // [] == false
+// "" = 0
+
+// console.log(Number(y))
+// var z = [null,undefined] //=> "[nul]"
+// console.log(String(z))
+
+// [[],[],[]] => '[]' ,,,
+
+// {} => '{}'  objec Obj
+
+// var v = {}
+// console.log(String(v))
+
+// Number
+
+// '.' => nan 
+
+// false => 0
+
+// console.log(Number(null))
+
+// true => 1
+
+// undefined = Nan
+
+// null = 0
+
+
+// [""] => 0
+// console.log(Number([""]))
+// let a = "5"
+// var b = a = a + 1
+// // console.log(Number(a))  //'501'
+// console.log(Number(+a))  //'501'
+// // console.log(Number(a))  //'501'
+
+// +a
+// console.log(typeof (parseInt("10")))
+
+
+// console.log(a)
+
+// console.log(a)
+
+// console.log(a)
+
+// console.log(a)
+
+
+
+// console.log(3 > 2 > 1) // false
+// console.log(1 > 2 > 3) // false
+
+
+// var a = 10
+// console.log(typeof a)
+// console.log(typeof (typeof a))
+// console.log(typeof(typeof (typeof a)))
+
+
+// var a = abv;
+// console.log(typeof(a));
+
+
+
+// var a = "Danish"
+// var b =a.length
+// console.log(typeof b)
+
+
+// console.log(ii)
+
+// const ii = 10
+
+
+
+
+// var a = 10
+// console.log(a);//10
+// a++//10
+// console.log(a)//11
+// ++a//12
+// console.log(a)//12
+
+
+// const arr = new Array(1,2,3,4,5)
+// console.log(arr)
